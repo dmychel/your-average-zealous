e3e6fce29acdfdc64e224dfdd666412f04625058
dmychel <dmychel@tutanota.com>
2024-09-26 18:41:41 -0400

    Map images
    Created array with list of links to images.
    Function maps out array with image element.

--- src/routes/ArtDesign.jsx
+++ src/routes/ArtDesign.jsx
@@ -1,10 +1,22 @@
 import "/src/styles/global.scss";
 
 export default function ArtDesign() {
+  const arr = [
+    "https://dmychel.github.io/zealout-images/art_design/ed.png",
+    "https://dmychel.github.io/zealout-images/art_design/TORQUE24.png",
+    "https://dmychel.github.io/zealout-images/art_design/alejandro.png",
+    "https://dmychel.github.io/zealout-images/art_design/first success.png",
+    "https://dmychel.github.io/zealout-images/art_design/mac.png",
+  ];
+
   return (
     <div className="index content">
       <h2>Art and Design</h2>
-      <div className="content"></div>
+      <div className="content">
+        {arr.map((img, index) => (
+          <img src={img} alt="error" key={index} />
+        ))}
+      </div>
     </div>
   );
 }
