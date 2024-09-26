2b858aed9e27863ed1f39ab84b2556f15c21fdf2
dmychel <dmychel@tutanota.com>
2024-09-26 19:04:30 -0400

    arr update

--- src/routes/ArtDesign.jsx
+++ src/routes/ArtDesign.jsx
@@ -2,17 +2,17 @@ import "/src/styles/global.scss";
 
 export default function ArtDesign() {
   const arr = [
+    "https://dmychel.github.io/zealout-images/art_design/mac.png",
     "https://dmychel.github.io/zealout-images/art_design/ed.png",
     "https://dmychel.github.io/zealout-images/art_design/TORQUE24.png",
-    "https://dmychel.github.io/zealout-images/art_design/alejandro.png",
     "https://dmychel.github.io/zealout-images/art_design/neo.png",
-    "https://dmychel.github.io/zealout-images/art_design/mac.png",
+    "https://dmychel.github.io/zealout-images/art_design/alejandro.png",
   ];
 
   return (
-    <div className="index">
+    <div className="content">
       <h2>Art and Design</h2>
-      <div className="content gallery">
+      <div className="gallery">
         {arr.map((img, index) => (
           <img src={img} alt="error" key={index} />
         ))}
