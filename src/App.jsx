677ce17da3aae628065a35e9cb9f9035b91604fd
dmychel <dmychel@tutanota.com>
2024-09-26 17:19:32 -0400

    imported style sheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SideNav from "./components/SideNav";
 import Index from "./components/routes/Index";
+import "/src/styles/index.scss";
 
 function App() {
   return (
