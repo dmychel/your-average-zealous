b1959a02adda1fc253d53ac4ff373d144a4a8a33
dmychel <dmychel@tutanota.com>
2024-09-26 18:55:27 -0400

    changed a link in array and added class to jsx

--- src/routes/ArtDesign.jsx
+++ src/routes/ArtDesign.jsx
@@ -5,14 +5,14 @@ export default function ArtDesign() {
     "https://dmychel.github.io/zealout-images/art_design/ed.png",
     "https://dmychel.github.io/zealout-images/art_design/TORQUE24.png",
     "https://dmychel.github.io/zealout-images/art_design/alejandro.png",
-    "https://dmychel.github.io/zealout-images/art_design/first success.png",
+    "https://dmychel.github.io/zealout-images/art_design/neo.png",
     "https://dmychel.github.io/zealout-images/art_design/mac.png",
   ];
 
   return (
-    <div className="index content">
+    <div className="index">
       <h2>Art and Design</h2>
-      <div className="content">
+      <div className="content gallery">
         {arr.map((img, index) => (
           <img src={img} alt="error" key={index} />
         ))}
