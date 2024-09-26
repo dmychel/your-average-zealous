5c4e4c5f936463f488853ca96abb96feacfae6cb
dmychel <dmychel@tutanota.com>
2024-09-26 17:19:54 -0400

    added jsx and imported style sheet

--- src/components/routes/Index.jsx
+++ src/components/routes/Index.jsx
@@ -1,3 +1,10 @@
+import "/src/styles/global.scss";
+
 export default function Index() {
-  return;
+  return (
+    <div className="index content">
+      <h2>Mix of Art and Design</h2>
+      <div className="content"></div>
+    </div>
+  );
 }
